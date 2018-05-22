d986c5be864084229a669068c40c0ac36264f7de
giladiniv <giladiniv@cs.technion.ac.il>
2018-05-22 17:11:46 +0300

    bug fix

--- models/simplenet.py
+++ models/simplenet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class SimpleNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(SimpleNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
