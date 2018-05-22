15265362b9c7a1b37025f9bab9f02422ddf6c322
Niv Giladi <giladiniv@csf.technion.ac.il>
2018-05-22 17:05:27 +0300

    testing git

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@ from models.resnet import resnet
 from models.densenet import densenet
 from models.alexnet import alexnet
 from models.wideresnet import WideResNet
+from models.simplenet import SimpleNet
 
 
 def get_model(model, args):
@@ -12,5 +13,7 @@ def get_model(model, args):
     if model == 'wideresnet':
         return WideResNet(args.layers, args.dataset == 'cifar10' and 10 or 100,
                           args.widen_factor, dropRate=args.droprate, gbn=args.gbn)
+    if model == 'simplenet':
+        return SimpleNet()
     if model == 'densenet':
         return densenet()
