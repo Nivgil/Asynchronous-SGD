1c4abe1101ff253d08e287ba8e80c8f771c936e8
giladiniv <giladiniv@cs.technion.ac.il>
2018-06-24 11:49:58 +0300

    wide resenet bn momentum support

--- gbn.py
+++ gbn.py
@@ -4,9 +4,9 @@ from torch.nn import Module, BatchNorm2d
 
 class GhostBatchNorm(Module):
 
-    def __init__(self, num_features, chunk_size=128):
+    def __init__(self, num_features, chunk_size=128, momentum=0.1):
         super(GhostBatchNorm, self).__init__()
-        self.bn = BatchNorm2d(num_features)
+        self.bn = BatchNorm2d(num_features, momentum=momentum)
         self.num_features = num_features
         self.chunk_size = chunk_size
         self.weight = self.bn.weight
