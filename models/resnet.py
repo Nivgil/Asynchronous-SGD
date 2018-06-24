cf6c3220430b16ca886abe12d1a9a24ec09ab77a
giladiniv <giladiniv@cs.technion.ac.il>
2018-06-24 19:12:25 +0300

    wide resenet bn momentum support

--- models/resnet.py
+++ models/resnet.py
@@ -56,17 +56,17 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, bn_momentum=0.1):
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=bn_momentum)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.bn1.weight.data.zero_().add_(1)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=bn_momentum)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.bn2.weight.data.zero_().add_(1)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4, momentum=bn_momentum)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
         self.bn3.weight.data.zero_()
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -100,20 +100,20 @@ class ResNet(nn.Module):
     def __init__(self):
         super(ResNet, self).__init__()
 
-    def _make_layer(self, block, planes, blocks, stride=1, bn_momentum=0.1):
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion, momentum=bn_momentum),
+                nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, bn_momentum))
+        layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, bn_momentum))
+            layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
@@ -128,18 +128,18 @@ class ResNet(nn.Module):
 class ResNet_imagenet(ResNet):
 
     def __init__(self, num_classes=1000,
-                 block=Bottleneck, layers=[3, 4, 23, 3], bn_momentum=0.1):
+                 block=Bottleneck, layers=[3, 4, 23, 3]):
         super(ResNet_imagenet, self).__init__()
         self.inplanes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = nn.BatchNorm2d(64, momentum=bn_momentum)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], bn_momentum=bn_momentum)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, bn_momentum=bn_momentum)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, bn_momentum=bn_momentum)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, bn_momentum=bn_momentum)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7)
         self.feats = nn.Sequential(self.conv1,
                                    self.bn1,
