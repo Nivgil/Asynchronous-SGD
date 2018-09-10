4e088f1933c0bbf9e9a18f14da1f09e023333b25
giladiniv <giladiniv@cs.technion.ac.il>
2018-09-11 01:08:24 +0300

    stable version

--- models/densenet.py
+++ models/densenet.py
@@ -76,7 +76,7 @@ class Transition(nn.Module):
 class DenseNet(nn.Module):
 
     def __init__(self, depth=190, block=Bottleneck,
-                 dropRate=0, num_classes=10, growthRate=40, compressionRate=2):
+                 dropRate=0, num_classes=10, growthRate=24, compressionRate=2):
         super(DenseNet, self).__init__()
 
         assert (depth - 4) % 3 == 0, 'depth should be 3n+4'
